1b16c99e489cdf25a9ca7809289992984b80cb2b
Jabbath <antonafana@yahoo.ca>
2014-10-21 16:30:49 -0230

    First commit, semi-working

---
+++ contTest.js
@@ -0,0 +1,81 @@
+var HID = require('node-hid');
+
+function usbController(pid,vid,config){
+	var controller = new HID.HID(pid,vid);
+	var eventTriggers = [];
+	var previousValue;
+	
+	var reduceToConfVals = function(data,position){
+		var keys = [];
+		
+		//TODO: we have to find the greatest possible value each time
+		
+		while(data !== 0){
+			var greatest = [0,'key'];
+		
+			for(var prop in config){
+				var curKey = config[prop]; 
+				
+				if(curKey.type === 'button' && curKey.pos === position && curKey.val <= data && curKey>greatest[0]){
+					greatest[0] = curKey.val;
+					greatest[1] = prop;
+					keys.push(prop);
+					data-= curKey.val;
+				}
+			}
+			keys.push(greatest[1]);
+			data-= greatest[0];
+		}
+		
+		return keys;
+	}
+	
+	var eventHandler = function(data,eventTrigger){
+		var trigger = eventTrigger.eventTrigger;
+		//console.log('triggered',trigger);
+		var position = config[trigger].pos;
+		var type = config[trigger].type;
+		
+		if(type === 'button'){
+			var buttonVals = reduceToConfVals(data,position); //When multiple button are pressed we have to separate their values
+			console.log(buttonVals);
+			
+			for(var i=0;i<buttonVals.length;i++){
+				if(buttonVals[i] === trigger){
+					eventTrigger.callback(data);
+				}
+			}
+			
+		}
+		else if(type === 'axis'){
+			eventTrigger.callback(data);
+		}
+	};
+	
+	controller.on('data',function(data){
+		data = data.toJSON(); //By default, data is a Buffer object so we need to convert to an Array
+		
+		if(previousValue){
+			//console.log('prevVal',previousValue);
+			for(var i=0;i<eventTriggers.length;i++){
+				var eventTrigger = eventTriggers[i].eventTrigger;
+				var position = config[eventTrigger].pos;
+				var callback = eventTriggers[i].callback;
+			
+				//if(config[eventTrigger].hasOwnProperty('defaultVal')){//Some inputs are not at 0 by default (perhaps not needed) try only checking for change
+				if(data[position] !== previousValue[position]){//currently only checking for change
+					eventHandler(data[position],eventTriggers[i]);
+				}
+			}
+		}
+		previousValue = data;
+		return false;
+	});
+	
+	this.on = function(eventTrigger,callback){
+		eventTriggers.push({'eventTrigger': eventTrigger,'callback': callback});
+	};
+}
+
+module.exports = usbController;
+
