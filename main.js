93776f60eb504d7fe522796380ed3aae86043aad
Jabbath <antonafana@yahoo.ca>
2014-10-23 19:22:41 -0230

    Removed unnecessary function

--- main.js
+++ main.js
@@ -4,31 +4,7 @@ function usbController(pid,vid,config){
 	var controller = new HID.HID(pid,vid);
 	var eventTriggers = [];
 	var previousValue;
-	
-	var reduceToConfVals = function(data,position){
-		var keys = [];
 		
-		while(data !== 0){
-			var greatest = [0,'key'];
-		
-			for(var prop in config){
-				var curKey = config[prop]; 
-				
-				//We have to find the greatest button value that can fit into the data
-				
-				if(curKey.type === 'button' && curKey.pos === position && curKey.val <= data && curKey.val>greatest[0]){
-					greatest[0] = curKey.val;
-					greatest[1] = prop;
-				}
-			}
-			keys.push(greatest[1]);
-			data-= greatest[0]; //We subtract this value from the data so we can find the second greatest value that will fit
-			//console.log(greatest);
-		}
-		
-		return keys;
-	}
-	
 	var eventHandler = function(data,eventTrigger){
 		//console.log('eventHandler',eventTrigger);
 		var trigger = eventTrigger.eventTrigger;
@@ -36,41 +12,23 @@ function usbController(pid,vid,config){
 		var type = config[trigger].type;
 		
 		if(type === 'button'){
-			var buttonVals = reduceToConfVals(data,position); //When multiple button are pressed we have to separate their values
-			var previousButtonVals = reduceToConfVals(previousValue[position],position);
+			var currentVal = data[position] & config.trigger.val; //When multiple button are pressed we have to separate their values
+			var previousVal = previousValue[position] & config.trigger.val;
 			//console.log(previousVals,'previousValue');
 			
-			for(var i=0;i<buttonVals.length;i++){
-				var pressedLast = false;
-				
-				/*We need to check if the button was already pressed last
-				time around so the callback does not refire if a second button
-				is pressed before the first is release*/
-				
-				
-				for(var l=0;l<previousButtonVals.length;l++){ 
-					//console.log(previousVals,trigger);
-					if(previousButtonVals[l]===trigger){
-						pressedLast = true;
-						//console.log(pressedLast);
-					}
-				}
-				
-				if(buttonVals[i] === trigger && !pressedLast){
-					eventTrigger.callback(data);
-				}
+			if(currentVal && !previousVal){
+				eventTrigger.callback(data[position]);
 			}
 			
 		}
 		else if(type === 'axis'){
-			eventTrigger.callback(data);
+			eventTrigger.callback(data[position]);
 		}
 	};
 	
 	controller.on('data',function(data){
-		data = data.toJSON(); //By default, data is a Buffer object so we need to convert to an Array
 		
-		if(previousValue){
+		if(previousValue){//This is undefined first data event
 			//console.log('prevVal',previousValue);
 			for(var i=0;i<eventTriggers.length;i++){
 				var eventTrigger = eventTriggers[i].eventTrigger;
@@ -79,7 +37,7 @@ function usbController(pid,vid,config){
 			
 				if(data[position] !== previousValue[position]){//currently only checking for change
 					//console.log(data[position]);
-					eventHandler(data[position],eventTriggers[i]); //This is actually fired multiple times if multiple buttons share the same position
+					eventHandler(data,eventTriggers[i]); //This is actually fired multiple times if multiple buttons share the same position
 				}
 			}
 		}
@@ -93,4 +51,3 @@ function usbController(pid,vid,config){
 }
 
 module.exports = usbController;
-
